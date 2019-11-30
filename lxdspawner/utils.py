adb80bc0d723fd589190d1f947a81fa2d26766d0
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2019-11-30 11:28:47 +0900

    hardcode profile

--- lxdspawner/utils.py
+++ lxdspawner/utils.py
@@ -56,6 +56,7 @@ def start(client,
     container = client.containers.create({
         'name': container_name,
         'config': config,
+        'profiles': ['gpgpu']
         'source': {
             'type': 'image',
             # TODO(axw) make image alias configurable,
